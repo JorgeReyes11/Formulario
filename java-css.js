60f0ee0307fd27d58ad2542758005e83f4a2d7e7
Jorge <jorgejunior1970@hotmail.com>
2019-08-13 19:51:46 -0500

    Version 1 Formulario

---
+++ java-css.js
@@ -0,0 +1,7 @@
+$('.form-group').css({'margin-top':'30px'});
+$('body').css({'background-color':'steelblue'});
+$('#lblname').css({'font-weight':'bold'});
+$('#lblage').css({'font-weight':'bold'});
+$('#lblsex').css({'font-weight':'bold'});
+$('#lblmajor').css({'font-weight':'bold'});
+$('#lblstate').css({'font-weight':'bold'});
